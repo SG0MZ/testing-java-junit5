1a445f94d5571890e491d25108bc8e2aca153c1c
sagomezmartinez <sagomezmartinez@USQROSAGOMEZMA1.us.deloitte.com>
2023-09-11 13:43:16 -0500

    Advanced JUnit5 Testing - Second Commit

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,11 +2,17 @@ package guru.springframework.sfgpetclinic.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 
 import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import guru.springframework.ModelTests;
 
@@ -30,4 +36,35 @@ class OwnerTest implements ModelTests {
 		assertThat(owner.getCity(), is("Key West"));
 	}
 
+	@DisplayName("Value Source Test")
+	@ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+	@ValueSource(strings = {"Spring", "Framework", "Guru"})
+	void testValueSource(String val) {
+		System.out.println(val);
+	}
+	
+	@DisplayName("Enum Source Test")
+	@ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+	@EnumSource(OwnerType.class)
+	void enumTest(OwnerType ownerType) {
+		System.out.println(ownerType);
+	}
+	
+	@DisplayName("CSV Input Test")
+	@ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+	@CsvSource({
+			"FL, 1, 1",
+			"OH, 2, 2",
+			"MI, 1, 1"
+		})
+	void csvInputTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + " = " + val1 + ":" + val2);
+	}
+	
+	@DisplayName("CSV From File Test")
+	@ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+	@CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+	void csvFromFileTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + " = " + val1 + ":" + val2);
+	}
 }
